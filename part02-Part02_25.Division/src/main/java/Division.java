66513269c23af5c54b3ee56d5a5f2e37fd9e8729
bitterCell <76990920+bitterCell@users.noreply.github.com>
2021-01-05 20:38:16 +1030

    Add files via upload

---
+++ part02-Part02_25.Division/src/main/java/Division.java
@@ -0,0 +1,14 @@
+
+
+public class Division {
+
+    public static void main(String[] args) {
+        division(3, 5);
+        
+    }
+
+    public static void division(int numerator, int denominator) {
+        double divide = 1.0 * numerator / denominator;
+        System.out.println(divide);
+    }
+}
