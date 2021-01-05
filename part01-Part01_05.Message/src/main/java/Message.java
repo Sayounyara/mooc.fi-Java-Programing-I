fdabf95cfd3b5026d690734a0c327e1bcd80c3c6
bitterCell <76990920+bitterCell@users.noreply.github.com>
2021-01-05 18:27:10 +1030

    Add files via upload

---
+++ part01-Part01_05.Message/src/main/java/Message.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Message {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message:");
+        // Write your program here
+        String input = scanner.nextLine();
+        
+        System.out.println(input);
+        
+
+    }
+}
